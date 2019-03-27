89e6ffe31c445d9210a060e35211b3a696cf73a2
Jairo Robles Fiaga <roblesjf@bancoavvillas.com.co>
2019-03-27 16:06:24 -0500

    asd

--- BUA/Formato-BUA/src/app/app.module.ts
+++ BUA/Formato-BUA/src/app/app.module.ts
@@ -7,13 +7,14 @@ import { AppComponent } from "./app.component";
 import { LoginComponent } from "./login/login.component";
 import { LoginService } from "./login.service.service";
 import { Route } from "@angular/router";
+import { InicioComponent } from './inicio/inicio.component';
 
 const routes: Route[] = [
   { path: "login", component: LoginComponent, pathMatch: "full" }
 ];
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
+  declarations: [AppComponent, LoginComponent, InicioComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [LoginService],
   bootstrap: [AppComponent]

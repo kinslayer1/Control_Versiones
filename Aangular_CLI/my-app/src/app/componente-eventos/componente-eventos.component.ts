14185e3896b496efb29089ba9a61afcb723acc02
Jairo Robles Fiaga <roblesjf@bancoavvillas.com.co>
2019-03-27 11:01:26 -0500

    c

--- Aangular_CLI/my-app/src/app/componente-eventos/componente-eventos.component.ts
+++ Aangular_CLI/my-app/src/app/componente-eventos/componente-eventos.component.ts
@@ -6,7 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./componente-eventos.component.css']
 })
 export class ComponenteEventosComponent implements OnInit {
+ users: string[] =['lola','santi'];
+ name: string='Jhon';
+ edad: number=20;
 
+ sayhello(){
+   alert('Hello');
+ }
+ borrarusuario(user){
+   for(let i=0;i<this.users.length;i++){
+     if(user==this.users[i]){
+       this.users.splice(i,1);
+     }
+   }
+ }
+ agregarusuario(newuser){
+  this.users.push(newuser.value);
+  newuser.value='';
+  newuser.focus();
+  return false;
+ }
   constructor() { }
 
   ngOnInit() {

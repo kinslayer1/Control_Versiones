a5e9a187a0a59d1375bd7e0fa147efb41c7bdac8
Jairo Robles Fiaga <roblesjf@bancoavvillas.com.co>
2019-04-03 13:51:41 -0500

    ASD

--- BUA/Formato-BUA/src/app/contrucal/contrucal.component.ts
+++ BUA/Formato-BUA/src/app/contrucal/contrucal.component.ts
@@ -1,18 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import {Router} from "@angular/router"
+import { NgForm } from '@angular/forms/src/directives/ng_form';
 @Component({
   selector: 'app-contrucal',
   templateUrl: './contrucal.component.html',
   styleUrls: ['./contrucal.component.css']
 })
 export class ContrucalComponent implements OnInit {
+  @Input() pisos;
 
   constructor(private router: Router) { }
-
   ngOnInit() {
   }
-  nextPage(){   
+  nextPage(contactForm){
+    alert("pisos:"+contactForm.pisos.value + contactForm.muros.value
+    +contactForm.carpmetal.value
+    +contactForm.cocina.value
+    +contactForm.techos.value
+    +contactForm.carpmadera.value
+    +contactForm.banos.value );
     this.router.navigate(['/prophorz']);
    return false;
   }
+
+
 }

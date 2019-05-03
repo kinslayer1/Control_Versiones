f92ff0380f69f228e8b9bc501b4a21343152e2ae
Jairo Robles Fiaga <roblesjf@bancoavvillas.com.co>
2019-05-03 09:05:47 -0500

    ASD

--- API_REST/TodoApi/Startup.cs
+++ API_REST/TodoApi/Startup.cs
@@ -10,8 +10,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Microsoft.EntityFrameworkCore;
-using TodoApi.Models;
 
 namespace TodoApi
 {
@@ -27,8 +25,6 @@ namespace TodoApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<TodoContext>(opt =>
-                opt.UseInMemoryDatabase("TodoList"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 

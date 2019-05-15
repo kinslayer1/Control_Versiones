c76198bbf61b36be5a146c6c9b15d14ae055927a
kinslayer1 <jairo0914@gmail.com>
2019-05-15 10:44:33 -0500

    ASD

--- BUA/Formato-BUA/src/environments/environment.prod.ts
+++ BUA/Formato-BUA/src/environments/environment.prod.ts
@@ -1,10 +1,15 @@
-//DECLARACION DE VARIABLES PARA AMBIENTE DE PRODUCCION
+//DECLARACION DE VARIABLES PARA AMBIENTE DE PRUEBAS
 
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
 import { HttpHeaders } from "@angular/common/http";
 export const environment = {
-  production: true,
+  production: false,
   //END-POINT del servicio de autenticacion
   serviceEndpoint: "http://canaloficinas02desa:7003/PJBA_ManejoIdentidad_AutenticarRest/resources/",
+  azureFunction: "http://canaloficinas02desa:7003/PJBA_ManejoIdentidad_AutenticarRest/resources/",
+  avaluoPerito: "http://localhost:8080/api/avaluo",
   //HEADERS de opciones
   httpOptions: {
     headers: new HttpHeaders({
@@ -15,14 +20,26 @@ export const environment = {
       'Content-Type':  'application/json'
     })      
   },
-  //Variables tipo de peticion para el servicio de autenticacion
+  //Constantes de la aplicacion
   autenticar:"Autenticar",
   autorizar:"Autorizar",
-  //Variables respuesta del servicio de autenticacion
+  //Variables de la aplicacion
   codRespuesta:"",
   descrRespuesta:"",
   tokenAutorizacion:"",
+  token:"",
+  nombre:"",
+  exp:"",
   grupos:"",
   grupo:"",
   tokenGrupo:""
 };
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.

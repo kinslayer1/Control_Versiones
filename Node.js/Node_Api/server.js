ea626b9eb1bdad15f1fe1a8ddaf19002ab058657
kinslayer1 <jairo0914@gmail.com>
2019-05-28 13:23:56 -0500

    asd

---
+++ Node.js/Node_Api/server.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var http = require('http')
+var app = express()
+ 
+var users = ['oscar', 'juan', 'marcos']
+ 
+app.get('/users', function(req, res) {
+  res.send(users)
+})
+ 
+app.get('/', function(req, res) {
+  res.status(200).send("Welcome to API REST")
+})
+ 
+http.createServer(app).listen(443, function()  {
+  console.log('Server started at http://localhost:443');
+});

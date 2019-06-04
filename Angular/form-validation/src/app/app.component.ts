f475305a04e5b07d3e020f6210056ef257819a1e
kinslayer1 <jairo0914@gmail.com>
2019-06-04 16:19:44 -0500

    deletecomm

--- Angular/form-validation/src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  template: `<app-hero-form-template></app-hero-form-template>
-             <hr>
-             <app-hero-form-reactive></app-hero-form-reactive>`
-})
-export class AppComponent { }
